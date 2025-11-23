b73a6157f6cd8e5dc717e54eefe894e5162c947c
flyingt0951497-sudo <flyingt09.51497@gmail.com>
2025-11-23 11:46:55 +0700

    Create sdfgh.py

---
+++ sdfgh.py
@@ -0,0 +1,8 @@
+log_entries = []
+
+    for root, dirs, files in os.walk(base_path):
+        for file in files:
+            if file == target_filename:
+                full_path = os.path.normpath(os.path.join(root, file))
+                try:
+                    with open(full_path, "r", encoding="utf-8") as f:
